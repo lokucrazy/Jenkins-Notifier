0c150e44868b206e874cc673b2d1d2a57488437e
Ryan Lokugamage <erangal@outlook.com>
2019-08-11 20:22:25 -0500

    Initial Commit

---
+++ JenkinsNotifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsNotifier
+//
+//  Created by Ryan Lokugamage on 8/11/19.
+//  Copyright © 2019 Ryan Lokugamage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
